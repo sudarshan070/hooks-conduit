e147613c8254cab37b842a87db477692f1ce2ee8
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-10-08 18:27:14 +0530

    header style done

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <h1>In Header</h1>
+    <div className="d-flex justify-content-between align-items-center p-3 border-bottom bg-light">
+      <div className="logo">
+        <h1>Conduit</h1>
+      </div>
+      <div>
+        <button type="button" className="btn btn-outline-primary">
+          Login
+        </button>
+        <button type="button" className="btn btn-outline-success ml-3">
+          SignUp
+        </button>
+      </div>
     </div>
   );
 }
